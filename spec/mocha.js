e87c7b4da3c012d954bc77e8bde0bf005d90d4cc
dpilafian <dem@centerkey.com>
2021-06-11 02:46:24 -0700

    Add semver test case

--- spec/mocha.js
+++ spec/mocha.js
@@ -5,10 +5,23 @@
 import assert from 'assert';
 
 // Setup
-import { w3cHtmlValidator } from '../build/w3c-html-validator.js';
+import { w3cHtmlValidator } from '../dist/w3c-html-validator.js';
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-describe('The library', () => {
+describe('Library version number', () => {
+
+   it('follows semantic version formatting', () => {
+      const data = w3cHtmlValidator.version;
+      const semVerPattern = /\d+[.]\d+[.]\d+/;
+      const actual =   { version: data, valid: semVerPattern.test(data) };
+      const expected = { version: data, valid: true };
+      assert.deepStrictEqual(actual, expected);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('Library', () => {
 
    it('has a validate() function', () => {
       const actual =   { validate: typeof w3cHtmlValidator.validate };
