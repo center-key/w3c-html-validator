894ed297857201ec3db5445862dbc7a699b780c4
dpilafian <dem@centerkey.com>
2021-06-12 23:11:30 -0700

    Replace callback with promise

--- spec/mocha.js
+++ spec/mocha.js
@@ -3,9 +3,12 @@
 
 // Imports
 import assert from 'assert';
+import { readFileSync } from 'fs';
 
 // Setup
 import { w3cHtmlValidator } from '../dist/w3c-html-validator.js';
+const validHtml =   readFileSync('spec/html/valid.html',   'utf8');
+const invalidHtml = readFileSync('spec/html/invalid.html', 'utf8');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 describe('Library version number', () => {
@@ -21,12 +24,195 @@ describe('Library version number', () => {
    });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-describe('Library', () => {
+describe('Library module', () => {
+
+   it('is an object', () => {
+      const actual =   { constructor: w3cHtmlValidator.constructor.name };
+      const expected = { constructor: 'Object' };
+      assert.deepStrictEqual(actual, expected);
+      });
 
    it('has a validate() function', () => {
       const actual =   { validate: typeof w3cHtmlValidator.validate };
       const expected = { validate: 'function' };
       assert.deepStrictEqual(actual, expected);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('Pretty-Print JSON website', () => {
+
+   it('validates', (done) => {
+      const handleData = (data) => {
+         const actual = data;
+         const expected = {
+            validates: true,
+            mode:      'website',
+            html:      null,
+            filename:  null,
+            website:   'https://pretty-print-json.js.org/',
+            output:    'json',
+            status:    200,
+            messages:  [],
+            display:   null,
+            };
+         assert.deepStrictEqual(actual, expected);
+         done();
+         };
+      w3cHtmlValidator.validate({ website: 'https://pretty-print-json.js.org/' }).then(handleData);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('Valid HTML string', () => {
+
+   it('passes validator with JSON output', (done) => {
+      const handleData = (data) => {
+         const actual = data;
+         const expected = {
+            validates: true,
+            mode:      'html',
+            html:      validHtml,
+            filename:  null,
+            website:   null,
+            output:    'json',
+            status:    200,
+            messages:  [],
+            display:   null,
+            };
+         assert.deepStrictEqual(actual, expected);
+         done();
+         };
+      w3cHtmlValidator.validate({ html: validHtml, output: 'json' }).then(handleData);
+      });
+
+   it('passes validator with HTML output', (done) => {
+      const handleData = (data) => {
+         const actual = data;
+         delete actual.display;
+         const expected = {
+            validates: true,
+            mode:      'html',
+            html:      validHtml,
+            filename:  null,
+            website:   null,
+            output:    'html',
+            status:    200,
+            messages:  null,
+            };
+         assert.deepStrictEqual(actual, expected);
+         done();
+         };
+      w3cHtmlValidator.validate({ html: validHtml, output: 'html' }).then(handleData);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('Invalid HTML string', () => {
+
+   it('fails validator with JSON output', (done) => {
+      const handleData = (data) => {
+         const actual = data;
+         const expected = {
+            validates: false,
+            mode:      'html',
+            html:      invalidHtml,
+            filename:  null,
+            website:   null,
+            output:    'json',
+            status:    200,
+            messages:  [
+               {
+                  type:         'info',
+                  lastLine:     9,
+                  lastColumn:   12,
+                  firstColumn:  4,
+                  subType:      'warning',
+                  message:      'Section lacks heading. Consider using “h2”-“h6” elements to add identifying headings to all sections.',
+                  extract:      'e</h1>\n   <section>\n     ',
+                  hiliteStart:  10,
+                  hiliteLength: 9,
+                  },
+               {
+                  type:         'error',
+                  lastLine:     12,
+                  lastColumn:   21,
+                  firstColumn:  10,
+                  message:      'Element “blockquote” not allowed as child of element “span” in this context. (Suppressing further errors from this subtree.)',
+                  extract:      '\n   <span><blockquote>Inside',
+                  hiliteStart:  10,
+                  hiliteLength: 12,
+                  },
+               ],
+            display:   null,
+            };
+         assert.deepStrictEqual(actual, expected);
+         done();
+         };
+      w3cHtmlValidator.validate({ html: invalidHtml, output: 'json' }).then(handleData);
+      });
+
+   it('fails validator with HTML output', (done) => {
+      const handleData = (data) => {
+         const actual = data;
+         delete actual.display;
+         const expected = {
+            validates: false,
+            mode:      'html',
+            html:      invalidHtml,
+            filename:  null,
+            website:   null,
+            output:    'html',
+            status:    200,
+            messages:  null,
+            };
+         assert.deepStrictEqual(actual, expected);
+         done();
+         };
+      w3cHtmlValidator.validate({ html: invalidHtml, output: 'html' }).then(handleData);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('Valid HTML file', () => {
+
+   it('passes validator', (done) => {
+      const handleData = (data) => {
+         const actual = data;
+         const expected = {
+            validates: true,
+            mode:      'filename',
+            html:      validHtml,
+            filename:  'spec/html/valid.html',
+            website:   null,
+            output:    'json',
+            status:    200,
+            messages:  [],
+            display:   null,
+            };
+         assert.deepStrictEqual(actual, expected);
+         done();
+         };
+      w3cHtmlValidator.validate({ filename: 'spec/html/valid.html' }).then(handleData);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('Invalid HTML file', () => {
+
+   it('fails validator', (done) => {
+      const handleData = (data) => {
+         const actual =   { validates: data.validates };
+         const expected = { validates: false };
+         assert.deepStrictEqual(actual, expected);
+         done();
+         };
+      w3cHtmlValidator.validate({ filename: 'spec/html/invalid.html' }).then(handleData);
       });
 
    });
--- w3c-html-validator.ts
+++ w3c-html-validator.ts
@@ -1,60 +1,76 @@
 // W3C HTML Validator ~ MIT License
 
 import { readFileSync } from 'fs';
-import * as request from 'superagent';
-import withProxy from 'superagent-proxy';
-withProxy(request);
+import request from 'superagent';
 
 export type ValidatorOptions = {
-   file?:     string,
-   input?:    string,
+   html?:     string,  //example: '<!doctype html><html><head><title>Home</title></html>''
+   filename?: string,  //example: 'docs/index.html'
+   website?:  string   //example: 'https://pretty-print-json.js.org/'
    checkUrl?: string,
-   output?:   'json' | 'html',
-   proxy?:    string,
-   callback?: (response: unknown, info?: unknown) => void,
+   output?:   ValidatorResults['output'],
+   };
+export type ValidatorResultsMessage = {
+   type:         'info' | 'error',
+   lastLine:     number,
+   lastColumn:   number,
+   firstColumn:  number,
+   subType?:     'warning',
+   message:      string,
+   extract:      string,
+   hiliteStart:  number,
+   hiliteLength: number,
+   };
+export type ValidatorResults = {
+   validates: boolean,
+   mode:      'html' | 'filename' | 'website';
+   html:      string | null,
+   filename:  string | null,
+   website:   string | null,
+   output:    'json' | 'html',
+   status:    number,
+   messages:  ValidatorResultsMessage[] | null,
+   display:   string | null,
    };
 
 const w3cHtmlValidator = {
 
    version: '[VERSION]',
 
-   validate(options: ValidatorOptions): void {
+   validate(options: ValidatorOptions): Promise<ValidatorResults> {
       const defaults = {
          checkUrl: 'https://validator.w3.org/nu/',
          output:   'json',
-         proxy:    null,
-         callback: (response: unknown) => console.log(response),
          };
       const settings = { ...defaults, ...options };
-      if (!settings.input && !settings.file)
-         throw Error('No "input" or "file" specified.');
-      const getRequest = (isLocal: boolean): request.SuperAgentRequest => {
-         const req = isLocal ? request.default.post(settings.checkUrl) :
-            request.default.get(settings.checkUrl);
-         if (settings.proxy)
-            req.proxy(settings.proxy);
-         req.set('User-Agent',   'w3c-html-validator');
-         req.set('Content-Type', 'text/html; encoding=utf-8');
-         return req;
-         };
-      const remoteMode =  settings.file && /^http[s]?:/.test(settings.file);
-      const type =        settings.input ? 'string' : remoteMode ? 'remote' : 'local';
-      const context =     settings.input || settings.file;
-      const req = getRequest(type !== 'remote');
-      req.query({ out: settings.output });
-      if (type === 'remote')
-         req.query({ doc: settings.file });
-      else
-         req.send(type === 'local' ? readFileSync(String(settings.file), 'utf8') : settings.input + '');
-      const handleResponse = (error: unknown, res: request.Response) => {
-         const getBody = () => {
-            res.body.context = context;
-            return res.body;
-            };
-         const getContext = () => settings.output === 'json' ? getBody() : res.text;
-         return error ? settings.callback(error) : settings.callback(null, getContext());
-         };
-      req.end(handleResponse);
+      if (!settings.html && !settings.filename && !settings.website)
+         throw Error('Must specify the "html", "filename", or "website" option.');
+      if (settings.output !== 'json' && settings.output !== 'html')
+         throw Error('Option "output" must be "json" or "html".');
+      const mode = settings.html ? 'html' : settings.filename ? 'filename' : 'website';
+      const readFile = () => settings.filename ? readFileSync(settings.filename, 'utf8') : null;
+      const inputHtml = settings.html || readFile();
+      const makePostRequest = () => request.post(settings.checkUrl)
+         .set('Content-Type', 'text/html; encoding=utf-8')
+         .send(<string>inputHtml);
+      const makeGetRequest = () => request.get(settings.checkUrl)
+         .query({ doc: settings.website });
+      const w3cRequest = inputHtml ? makePostRequest() : makeGetRequest();
+      w3cRequest.set('User-Agent', 'W3C HTML Validator ~ github.com/center-key/w3c-html-validator');
+      w3cRequest.query({ out: settings.output });
+      const json = settings.output === 'json';
+      const success = '<p class="success">';
+      return w3cRequest.then(response => ({
+         validates: json ? !response.body.messages.length : response.text.includes(success),
+         mode:      mode,
+         html:      inputHtml,
+         filename:  settings.filename || null,
+         website:   settings.website || null,
+         output:    <ValidatorResults['output']>settings.output,
+         status:    response.statusCode,
+         messages:  json ? response.body.messages : null,
+         display:   json ? null : response.text,
+         }));
       },
 
    };
