d0870a1c62a767fde321bdb481ed3067368c0767
dpilafian <dem@centerkey.com>
2024-01-03 16:59:45 -0800

    Release v1.6.3

--- dist/w3c-html-validator.d.ts
+++ dist/w3c-html-validator.d.ts
@@ -1,4 +1,4 @@
-//! w3c-html-validator v1.6.2 ~~ https://github.com/center-key/w3c-html-validator ~~ MIT License
+//! w3c-html-validator v1.6.3 ~~ https://github.com/center-key/w3c-html-validator ~~ MIT License
 
 export type ValidatorSettings = {
     html: string;
